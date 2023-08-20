0889a686f43252ec635e1ffbcf7e01c87382d8a3
Abolfazl Moslemian <moslemianabolfazl@gmail.com>
2023-08-20 10:56:25 +0330

    add log detail page

--- Pigeon/Pages/LogRequestDetail.cshtml.cs
+++ Pigeon/Pages/LogRequestDetail.cshtml.cs
@@ -4,33 +4,32 @@ using Pigeon.Factories;
 using Pigeon.Models;
 using Pigeon.Services;
 
-namespace Pigeon.Pages
+namespace Pigeon.Pages;
+
+public class LogRequestDetailModel : PageModel
 {
-    public class LogRequestDetailModel : PageModel
+    private readonly LogRequestService _logRequestService;
+    private readonly IndexPageFactory _indexPageFactory;
+
+    public LogRequestDetailModel(LogRequestService logRequestService,
+        IndexPageFactory indexPageFactory)
     {
-        private readonly LogRequestService _logRequestService;
-        private readonly IndexPageFactory _indexPageFactory;
+        _logRequestService = logRequestService;
+        _indexPageFactory = indexPageFactory;
+    }
 
-        public LogRequestDetailModel(LogRequestService logRequestService,
-            IndexPageFactory indexPageFactory)
-        {
-            _logRequestService = logRequestService;
-            _indexPageFactory = indexPageFactory;
-        }
+    public LogRequestModel LogRequest { get; set; }
 
-        public LogRequestModel LogRequest { get; set; }
+    public async Task<IActionResult> OnGet(int id)
+    {
+        var logRequest = await _logRequestService.GetLogRequestById(id, CancellationToken.None);
 
-        public async Task<IActionResult> OnGet(int id)
-        {
-            var logRequest = await _logRequestService.GetLogRequestById(id, CancellationToken.None);
+        if (logRequest is null)
+            return NotFound();
 
-            if (logRequest is null)
-                return NotFound();
+        LogRequest = await _indexPageFactory.PrepareLogRequestModel(logRequest!);
 
-            LogRequest = await _indexPageFactory.PrepareLogRequestModel(logRequest!);
+        return Page();
 
-            return Page();
-
-        }
     }
 }
