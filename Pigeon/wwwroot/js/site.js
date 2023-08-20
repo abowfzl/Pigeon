88c0d7065d1a0f65b5a0db60a8e7e73e13eb6e44
ardeshir <ardeshiir@outlook.com>
2023-08-20 16:13:26 +0000

    enahnce

--- Pigeon/wwwroot/js/site.js
+++ Pigeon/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const dataMiningForm = document.querySelector('#data-mining-form')
+const dataMiningInput = document.querySelector('#data-mining-input')
+
+dataMiningForm.addEventListener('submit',async(event)=>{
+    event.preventDefault()
+    const requestToken = dataMiningForm.children[1].value
+    const result = await fetch("/DataMining",{method:'POST',  headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },body:JSON.stringify({
+        Input: dataMiningInput.value,
+        __RequestVerificationToken:requestToken
+    })})
+    
+    const recivedString = result.join(',')
+    dataMiningInput.value += recivedString
+})
